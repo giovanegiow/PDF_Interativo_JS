1b9cb524da474ea1e45892c6efb912006c42a2f9
Giovane Nascimento <giovane.nascimento.giow@gmail.com>
2026-03-12 22:18:09 -0300

    Criado função para ler uma planilha excel

--- principal.js
+++ principal.js
@@ -1,10 +1,28 @@
 window.onload = function() {
-    const myModal = document.getElementById('myModal')
-    const myInput = document.getElementById('myInput')
+    document.getElementById('excelFile').addEventListener('change', (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
 
-    myModal.showModal();
+        reader.onload = (event) => {
+            const data = new Uint8Array(event.target.result);
+            const workbook = XLSX.read(data, { type: 'array' });
+            
+            // Lê a primeira planilha
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            
+            // Converte para JSON
+            const itensPlanilha = XLSX.utils.sheet_to_json(worksheet);
+            itensPlanilha.forEach(linha => {
+                const campos = Object.keys(linha);
 
-    myModal.addEventListener('shown.bs.modal', () => {
-        myInput.focus()
-    })
+                campos.forEach(nomeColuna => {
+                    const valor = linha[nomeColuna];
+
+                    alert (nomeColuna + '-' + valor);
+                })
+            })
+        };
+        reader.readAsArrayBuffer(file);
+    });
 }
